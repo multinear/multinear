c24d01a5a9b6872bb33ec340651ebfa516c29a94
Dima Kuchin <dima@kuchin.net>
2025-04-17 16:00:39 +0300

    Weighted score

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multinear',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(exclude=['*.frontend*', '*.templates*']),
     include_package_data=True,
     entry_points={
